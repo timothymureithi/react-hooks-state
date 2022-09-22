c5befc6e65839f92c8c49d274669b3b235e99382
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-22 10:52:37 +0300

    completed code lesson

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,37 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }
+
+ // return <div onClick={increment}>{count}</div>;
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
 
 export default Counter;
+
+/*
+
+function Counter() {
+ // const countState = useState(0);
+ // const count = countState[0];
+ // const setCount = countState[1];
+
+ const [count, setCount] = useState(0);
+
+ // button setter function. When clicked it increments count by 1
+  function increment() {
+    setCount(count + 1);
+    setCount(count + 1);
+  // setCount((currentCount) => currentCount + 1);
+  // setCount((currentCount) => currentCount + 1);
+
+
+  }
+
+  return <div onClick={increment}>{count}</div>;
+}
+
+*/
